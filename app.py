7ee6ed88c146ed087587bc2706836254d0a03a3b
Stef <stbuwalda@gmail.com>
2025-09-01 10:05:39 +0200

    Added an SQLite database that will host ping logs. DB file is automatically created and upgraded

--- app.py
+++ app.py
@@ -3,10 +3,31 @@ from flask import jsonify, Flask, render_template, send_file
 from poll_services import start_async_loop
 from mem import services
 import threading
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, init, upgrade
+from pathlib import Path
 
 # Flask app to serve status
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+db = SQLAlchemy(app=app)
+migration = Migrate(app=app, db=db)
+
+# Init and upgrade
+with app.app_context():
+    # Check if DB file or migrations folder is missing
+    if not (
+        Path("./instance/app.db").is_file() and Path("./migrations").is_dir()
+    ):
+        init()
+    # Upgrade db if any new migrations exist
+    upgrade()
+
+
+class logs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dateCreated = db.Column(db.DateTime, nullable=False)
 
 
 @app.route("/")
